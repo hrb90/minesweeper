be627206fba34f651dca7a0d37aa48b6fe2cc651
hrb90 <harrisonrbrown90@gmail.com>
2016-12-27 12:34:22 -0500

    add colorize in Tile#to_s

--- tile.rb
+++ tile.rb
@@ -1,3 +1,5 @@
+require 'colorize'
+
 class Tile
   attr_accessor :value
   attr_reader :revealed, :flagged
@@ -30,10 +32,35 @@ class Tile
   end
 
   def to_s
+    get_string.colorize(get_color)
+  end
+
+  def get_string
     return "F" if flagged
     return "*" unless revealed
     return "_" if empty?
     return "B" if is_bomb?
     value.to_s
+  end
+
+  def get_color
+    return :light_red if flagged
+    return :black unless revealed
+    return :white if empty?
+    return :black unless value.is_a?(Fixnum)
+    case value
+    when 1
+      :blue
+    when 2
+      :green
+    when 3
+      :red
+    when 4
+      :magenta
+    when 5
+      :yellow
+    else
+      :cyan
+    end
   end
 end
