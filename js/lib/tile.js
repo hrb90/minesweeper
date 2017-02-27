a1424867bac6c971b4636d579a9b4de5ceacf7af
hrb90 <harrisonrbrown90@gmail.com>
2017-02-27 10:15:49 -0500

    add board from forever ago

--- js/lib/tile.js
+++ js/lib/tile.js
@@ -5,6 +5,28 @@ class Tile {
     this.flagged = false;
   }
 
+  static makeGrid(rows, cols, numBombs) {
+    let bombArray = new Array(rows * cols);
+    bombArray.fill(false);
+    bombArray.fill(true, 0, numBombs);
+    function _shuffleBombs() {
+      for (let i = bombArray.length; i; i--) {
+        let j = Math.floor(Math.random() * i);
+        [bombArray[i - 1], bombArray[j]] = [bombArray[j], bombArray[i - 1]];
+      }
+    }
+    _shuffleBombs();
+    let grid = [];
+    for (let i = 0; i < rows; i++) {
+      let row = [];
+      for (let j = 0; j < cols; j++) {
+        row.push(new Tile(bombArray.pop()));
+      }
+      grid.push(row);
+    }
+    return grid;
+  }
+
   reveal () {
     this.unflag();
     this.revealed = true;
@@ -30,6 +52,6 @@ class Tile {
     if (this.flagged) return "f";
     if (!this.revealed) return "o";
     if (this.isBomb()) return "b";
-    return value;
+    return this.value;
   }
 }
